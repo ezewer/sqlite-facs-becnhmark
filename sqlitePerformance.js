f0410566fb87ce139da6014cf22b6b5cf65e055f
ezeswci <wernicke.ezequiel@gmail.com>
2020-10-30 19:03:10 +1100

    Check sqlite performance

---
+++ sqlitePerformance.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const sqlite = require('./sqlite')
+
+async function testSqlitePerformance (nums) {
+  // const nums = new Array(999).fill().map((v, i) => i) // 999
+  await sqlite.startSqlLite()
+  const oneByOneTakes = await testInsertOneByOne(nums)
+  console.log('oneByOneTakes: ', oneByOneTakes)
+  await sqlite.resetSqlLiteTable()
+  const parallelTakes = await testInsertParallel(nums)
+  console.log('parallelTakes: ', parallelTakes)
+  await sqlite.resetSqlLiteTable()
+  const instArrTakes = await testInsertArray(nums)
+  console.log('instArrTakes: ', instArrTakes)
+  for (let i = 0; i < 999; i++) {
+    await testInsertArray(nums)
+  }
+  const findTakes = await testFind()
+  console.log('findTakes: ', findTakes)
+  // Inserting the hole array at once is similar to performing just one insert.
+  // Parallel is faster than synchronous
+  return {
+    oneByOneTakes,
+    parallelTakes,
+    instArrTakes,
+    findTakes
+  }
+}
+
+async function testInsertOneByOne (nums) {
+  const start = Date.now()
+  for (const num of nums) {
+    await sqlite.insertNum(num)
+  }
+  const end = Date.now()
+  return end - start
+}
+
+async function testInsertParallel (nums) {
+  return new Promise((resolve, reject) => {
+    const start = Date.now()
+    Promise.all(
+      nums.map(num => sqlite.insertNum(num))
+    ).then((values) => {
+      const end = Date.now()
+      resolve(end - start)
+    })
+  })
+}
+
+async function testInsertArray (nums) {
+  if (nums.length > 999) throw new Error('Arr cant be bigger than 999')
+  const start = Date.now()
+  await sqlite.insertManyNums(nums)
+  const end = Date.now()
+  return end - start
+}
+
+async function testFind () {
+  const start = Date.now()
+  const res = await sqlite.findVals([1])
+  const end = Date.now()
+  if (res.length !== 1000) console.log('Error in res: ', res.length)
+  return end - start
+}
+
+module.exports = {
+  testSqlitePerformance
+}
